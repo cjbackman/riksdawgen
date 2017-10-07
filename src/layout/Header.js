3e7f5c4027d121a661c7fda4638761cedb429cb7
Vieriksson <me@viktoreriksson.se>
2017-10-07 16:00:05 +0200

    Fixed header overflow.

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,15 +2,5 @@ import React from 'react';
 import { Navbar } from '../components/Navbar.js';
 
 export const Header = () => (
-  <div style={styles.container}>
-    <Navbar />
-  </div>
+  <Navbar />
 );
-
-const styles = {
-  container: {
-    zIndex:10,
-    position: 'fixed',
-    width: '100%'
-  }
-}
