5d7b53567060e0eb07160fc17d948d47f5b26f73
Michael Andersson <mican817@gmail.com>
2017-10-10 22:46:10 +0200

    /api/personlista will now return unfiltered json upon call
    all .pyc files added to gitignore
    
    enabled flask internal server for easy debugging in development environment
    
    Todo:
    * Data is not cached yet. data.riksdagen.api is called at every call to /api/personlista.
    * Large set of data is returned. Consider filter data after purpose.

--- api/app.py
+++ api/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask import jsonify
+from personlista import personlista
+
 
 app = Flask(__name__)
+
+@app.route("/api/personlista")
+def get_personlista():
+	
+	PL = personlista()
+	return jsonify(personlista.fetch_data(PL))
 
 @app.route("/api")
 def welcome():
@@ -9,3 +18,10 @@ def welcome():
 @app.route("/api/hello")
 def hello():
     return "Hello World"
+
+# Only to enable easy debugging on host development machines, without running gunicorn server.
+# Do not use flask server in production...
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
