5d9b58020a20e65d8b7eb384129a402b12d9fd85
Vieriksson <me@viktoreriksson.se>
2017-10-11 20:00:42 +0200

    Merge probs.

--- api/app.py
+++ api/app.py
@@ -1,7 +1,16 @@
 """Test Python App"""
 from flask import Flask
+from flask import jsonify
+from personlista import personlista
+
 
 APP = Flask(__name__)
+
+@APP.route("/api/personlista")
+def get_personlista():
+
+	PL = personlista()
+	return jsonify(personlista.fetch_data(PL))
 
 @APP.route("/api")
 def welcome():
@@ -10,8 +19,11 @@ def welcome():
 
 @APP.route("/api/hello")
 def hello():
-    """Returns Hello World"""
     return "Hello World"
 
+# Only to enable easy debugging on host development machines, without running gunicorn server.
+# Do not use flask server in production...
 if __name__ == '__main__':
-    APP.run()
+    APP.run(debug=True)
+
+
