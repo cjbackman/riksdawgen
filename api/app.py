507fab9f09013c38fab0ee179736ff57e6f32eb5
root <eric.gratorp@gmail.com>
2017-10-22 13:30:19 +0100

    Initial flask restful API
    Change flask routes to flask restful counterpart

--- api/app.py
+++ api/app.py
@@ -1,14 +1,14 @@
 """Test Python App"""
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
 from flask_cors import CORS, cross_origin
 from members import members
 from redis import StrictRedis
 import json
 
-
 APP = Flask(__name__)
 CORS(APP)
+api = Api(APP)
 
 # Load configuration settings
 try:
@@ -21,6 +21,25 @@ except:
 REDIS = StrictRedis(host=APP.config[
                     'REDIS_SERVER'], port=6379, charset="utf-8", decode_responses=True)
 
+
+class members_API(Resource):
+        def get(self):
+                members = REDIS.get("members")
+                return jsonify(json.loads(members))
+
+class member_API(Resource):
+        def get(self, member_id):
+                member = REDIS.get(member_id)
+
+                if(member is None):
+                    member="{}"
+
+                return jsonify(json.loads(member))
+
+
+api.add_resource(members_API, '/api/members', endpoint='members')
+api.add_resource(member_API, '/api/member/<string:member_id>',
+                 endpoint='member')
 
 @APP.before_first_request
 def fetch_data():
@@ -40,28 +59,10 @@ def connect_to_data():
         print("MP is None, forced to fetch new. This shall not happen.")
         fetch_data()
 
-
-@APP.route('/api/member/<string:member_id>', methods=['GET'])
-def get_member(member_id):
-    member = REDIS.get(member_id)
-
-    if(member is None):
-        member = "{}"
-
-    return jsonify(json.loads(member))
-
-
-@APP.route("/api/members")
-def get_filt():
-    members_list = REDIS.get("members")
-    return jsonify(json.loads(members_list))
-
-
 @APP.route("/api")
 def welcome():
     """Welcome Text"""
     return "Welcome Riksdawgen's API! The end of the rainbow, where G&T brings you magic."
-
 
 @APP.route("/api/hello")
 def hello():
