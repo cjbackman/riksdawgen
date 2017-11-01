f629b20bb5dd6a72df239cede59abac7dc2b20a8
Carl-Johan Backman <caba@sto-m-caba.speedport.ip>
2017-11-01 13:05:59 +0100

    Adding travis config and pytest

---
+++ api/tests/stupid_test.py
@@ -0,0 +1,12 @@
+class TestClass(object):
+    def test_eken(self):
+        eken = 'diq'
+        assert eken == 'diq'
+
+    def test_gman(self):
+        gman_is_g = True
+        assert gman_is_g is True
+
+    def test_t(self):
+        tys_biceps_circumference = 5*1000
+        assert tys_biceps_circumference > 1000
