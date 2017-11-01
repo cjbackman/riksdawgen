9b6971d3555e17488ae6d673ea2df1789b4a50ba
Carl-Johan Backman <caba@sto-m-caba.speedport.ip>
2017-11-01 14:28:18 +0100

    Test pushing to docker hub

---
+++ deploy_dockerhub.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker-compose build --pull
+docker push riksdawgen_web
+docker push riksdawgen_api
