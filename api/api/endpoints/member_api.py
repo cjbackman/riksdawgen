076ac47cf6d33bc263d76d9d112b014db94e7050
Michael Andersson <mican817@gmail.com>
2017-11-09 21:52:38 +0100

    Added first prototype for api/member/voting/member_id

--- api/api/endpoints/member_api.py
+++ api/api/endpoints/member_api.py
@@ -4,6 +4,7 @@ from flask_restplus import Namespace, Resource, fields
 from flask import jsonify
 import json
 from api import REDIS
+from api.voting import voting
 
 ns = Namespace('api/member', description='Retreival of member data.')
 
@@ -21,4 +22,16 @@ class member_API(Resource):
             ns.abort(404, "Member not found")
 
         return jsonify(json.loads(member))
+
+@ns.route('/voting/<string:member_id>')
+class member_API(Resource):
+
+    @ns.response(200, 'OK.') #Documentation
+    @ns.response(404, 'Member not found.') #Documentation
+    def get(self, member_id):
+        """Retreive detailed data for a single member."""
+        V = voting()
+        raw_json = voting.get_data(V, member_id)
+
+        return jsonify(raw_json)
 
