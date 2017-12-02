79d5201e3a82c7c58a66b1d42aced6495fe6c971
Michael Andersson <mican817@gmail.com>
2017-12-02 13:32:53 +0100

    Parser for voting XML-file. No caching yet.

--- api/api/voting.py
+++ api/api/voting.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import urllib
+from urllib.request import urlopen
 import requests
 import json
 
@@ -43,11 +44,10 @@ class voting:
 
         output_list = []
         
-        print(number_of_votes)
         for i in range(0, number_of_votes):
             filtered_obj = {}
             filtered_obj['vote'] = voting_list[i]['rost']
-            filtered_obj['document_id'] = voting_list[i]['dok_id']
+            filtered_obj['document_id'] = voting_list[i]['dok_id']   # Will be overridden by id from XML-file
             filtered_obj['url_xml'] = voting_list[i]['votering_url_xml']
             filtered_obj['vote_id'] = voting_list[i]['votering_id']
             filtered_obj['name'] = voting_list[i]['namn']
@@ -60,8 +60,41 @@ class voting:
 
             filtered_obj['date'] = date
 
+            xml_data = self.parse_xml(filtered_obj['url_xml'])
+
+            for key, value in xml_data.items():
+                filtered_obj[key] = value
+
             output_list.append(filtered_obj)
 
         output_data = {'votes' : output_list}
 
         return output_data
+
+    def parse_xml(self, url):
+        """ Parses and return data from voting XML-file 
+        TODO: We must cache this data somehow. Currently redundant data is fetched. 
+        We could easily reduce the number of XML-files parsed by a factor of 10. """
+        import xml.etree.ElementTree as ET
+        
+        tree = ET.parse(urlopen(url))
+        root_tag = tree.getroot()
+        document_tag = root_tag.find('dokument')
+
+        tags = {'dok_id' : 'document_id',
+                'dokumentnamn' : 'doc_name',
+                'titel': 'title',
+                'publicerad' : 'published_date'}
+
+        fetched_data = {}
+
+        for key, new_key in tags.items():
+            try: 
+                data = document_tag.find(key).text
+            except:
+                data = None
+
+            fetched_data[new_key] = data
+
+        return fetched_data
+
